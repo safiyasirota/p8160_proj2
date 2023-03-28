4512cdb5f79d20839d6fa4f1a82cfa57fad8c59a
Bin Yang <97779585+byangstat@users.noreply.github.com>
2023-03-28 12:11:40 -0400

    update: use wave 2 data to standardize slope

--- start_value_wave_2.R
+++ start_value_wave_2.R
@@ -1,13 +1,19 @@
+# choose a start as two times the cumulative cases at around inflection point
 a_start <- 2 * max(df_wave$Y_i)
-# t0 can be some t_i near the inflection point
+
+# choose inflection point at around the end of the data set
 infl_point <- "2020-12-09"
+
+# choose k as the standardized rate of change at inflection point
 t0_start <- get_diff(start_date, infl_point)
-# k can be the slope near the inflection standardized by the cumulative cases near inflection point 
 lm_k <- lm(
   Y_i ~ t_i, data = df_wave %>% 
     filter(t_i %in% c(t0_start - 2, t0_start + 2))
 )
-k_start <- lm_k$coef[2] / df_wave_1 %>% filter(t_i == t0_start_1) %>% pull(Y_i) 
+k_start <- lm_k$coef[2] / df_wave %>% filter(t_i == t0_start) %>% pull(Y_i) 
+
+# choose d to be 0.5
 d_start <- 0.5
+
 # set starting theta vector
 start <- c(a_start, k_start, d_start, t0_start)
