c5d5fc198becc6aac99eff5929276198770afb29
Serena-Wang <yw4005@sph-yw4005-bio.local>
2023-03-29 00:35:41 -0400

    add second hosp wave fit

--- hosp_fit.R
+++ hosp_fit.R
@@ -194,3 +194,196 @@ hosp_wave1_nyc <- df_hosp_wave1_nyc %>%
 
 
 ggarrange(hosp_wave1_nyc, hosp_wave1_bx, hosp_wave1_bk, hosp_wave1_mn, hosp_wave1_qn, hosp_wave1_si,  nrow = 2, ncol = 3, common.legend = TRUE, legend = "bottom") 
+
+############################### WAVE 2 NOW ! #####################################
+## set start_date & end_date
+start_date <- "2020-09-15"
+end_date <- "2020-12-11"
+
+## prepare data (date, t_i, Y_i) for NYC
+df_hosp_wave2_nyc <- df %>%
+  filter(date >= start_date & date <= end_date) %>%
+  select(date:hospitalized_count) %>%
+  mutate(
+    t_i = get_diff(start_date, date),
+    Y_i = cumsum(hospitalized_count)
+  ) %>%
+  select(!hospitalized_count)
+
+## prepare data (date, t_i, Y_i) for each borough
+df_hosp_wave2_bx <- df %>%
+  filter(date >= start_date & date <= end_date) %>%
+  select(c(date, bx_hospitalized_count)) %>%
+  mutate(
+    t_i = get_diff(start_date, date),
+    Y_i = cumsum(bx_hospitalized_count)
+  ) %>%
+  select(!bx_hospitalized_count)
+
+df_hosp_wave2_bk <- df %>%
+  filter(date >= start_date & date <= end_date) %>%
+  select(c(date, bk_hospitalized_count)) %>%
+  mutate(
+    t_i = get_diff(start_date, date),
+    Y_i = cumsum(bk_hospitalized_count)
+  ) %>%
+  select(!bk_hospitalized_count)
+
+df_hosp_wave2_mn <- df %>%
+  filter(date >= start_date & date <= end_date) %>%
+  select(c(date, mn_hospitalized_count)) %>%
+  mutate(
+    t_i = get_diff(start_date, date),
+    Y_i = cumsum(mn_hospitalized_count)
+  ) %>%
+  select(!mn_hospitalized_count)
+
+df_hosp_wave2_qn <- df %>%
+  filter(date >= start_date & date <= end_date) %>%
+  select(c(date, qn_hospitalized_count)) %>%
+  mutate(
+    t_i = get_diff(start_date, date),
+    Y_i = cumsum(qn_hospitalized_count)
+  ) %>%
+  select(!qn_hospitalized_count)
+
+df_hosp_wave2_si <- df %>%
+  filter(date >= start_date & date <= end_date) %>%
+  select(c(date, si_hospitalized_count)) %>%
+  mutate(
+    t_i = get_diff(start_date, date),
+    Y_i = cumsum(si_hospitalized_count)
+  ) %>%
+  select(!si_hospitalized_count)
+
+## function to figure out starting parameters
+get_start <- function(df_wave, start_date = "2020-09-15", infl_point = "2020-12-09", d = 0.5) {
+  # a is just the maximum number of cumulative cases in the wave
+  a_start <- 2.2 * max(df_wave$Y_i)
+  # t0 can be some t_i near the inflection point
+  t0_start <- get_diff(start_date, infl_point)
+  lm_k <- lm(
+    Y_i ~ t_i, data = df_wave %>% 
+      filter(t_i %in% c(t0_start - 2, t0_start + 2))
+  )
+  k_start <- lm_k$coef[2] / df_wave %>% filter(t_i == t0_start) %>% pull(Y_i) 
+  # d can just be 0.5 
+  d_start <- d
+  # set starting theta vector
+  start <- c(a_start, k_start, d_start, t0_start)
+  return(start)
+}
+
+## NYC
+start_hosp_wave2_nyc <- get_start(df_wave = df_hosp_wave2_nyc)
+res_hosp_wave2_nyc <- NewtonRaphson(df_hosp_wave2_nyc, start_hosp_wave2_nyc)
+final_hosp_wave2_nyc <- res_hosp_wave2_nyc[nrow(res_hosp_wave2_nyc), 4:7]
+
+## apply newton-raphson to each borough
+## Bronx
+start_hosp_wave2_bx <- get_start(df_hosp_wave2_bx)
+res_hosp_wave2_bx <- NewtonRaphson(df_hosp_wave2_bx, start_hosp_wave2_bx)
+final_hosp_wave2_bx <- res_hosp_wave2_bx[nrow(res_hosp_wave2_bx), 4:7]
+
+## Brooklyn
+start_hosp_wave2_bk <- get_start(df_hosp_wave2_bk)
+res_hosp_wave2_bk <- NewtonRaphson(df_hosp_wave2_bk, start_hosp_wave2_bk)
+final_hosp_wave2_bk <- res_hosp_wave2_bk[nrow(res_hosp_wave2_bk), 4:7]
+
+## Manhattan
+start_hosp_wave2_mn <- get_start(df_hosp_wave2_mn)
+res_hosp_wave2_mn <- NewtonRaphson(df_hosp_wave2_mn, start_hosp_wave2_mn)
+final_hosp_wave2_mn <- res_hosp_wave2_mn[nrow(res_hosp_wave2_mn), 4:7]
+
+## Queens
+start_hosp_wave2_qn <- get_start(df_hosp_wave2_qn,infl_point = "2020-12-09")
+res_hosp_wave2_qn <- NewtonRaphson(df_hosp_wave2_qn, start_hosp_wave2_qn)
+final_hosp_wave2_qn <- res_hosp_wave2_qn[nrow(res_hosp_wave2_qn), 4:7]
+
+## Staten Island
+start_hosp_wave2_si <- get_start(df_hosp_wave2_si, d=0.25 )
+res_hosp_wave2_si <- NewtonRaphson(df_hosp_wave2_si, start_hosp_wave2_si)
+final_hosp_wave2_si <- res_hosp_wave2_si[nrow(res_hosp_wave2_si), 4:7]
+
+
+## plots
+## Bronx
+hosp_wave2_bx <- df_hosp_wave2_bx %>%
+  mutate(
+    Y_i_hat_start = N(t_i, start_hosp_wave2_bx),
+    Y_i_hat_final = N(t_i, final_hosp_wave2_bx)
+  ) %>%
+  pivot_longer(Y_i_hat_start:Y_i_hat_final, names_to = "type", values_to = "value") %>%
+  ggplot(aes(x = t_i)) + 
+  geom_point(aes(y = Y_i), size = 1) +
+  geom_line(aes(y = value, color = type)) +
+  labs(title = "Bronx", x ="Days (since Sep 15, 2020)", y = "Cumulative hospitalizations") +   
+  scale_color_discrete(name = "Fitted model", labels = c("Final", "Start")) 
+
+## Brooklyn
+hosp_wave2_bk <- df_hosp_wave2_bk %>%
+  mutate(
+    Y_i_hat_start = N(t_i, start_hosp_wave2_bk),
+    Y_i_hat_final = N(t_i, final_hosp_wave2_bk)
+  ) %>%
+  pivot_longer(Y_i_hat_start:Y_i_hat_final, names_to = "type", values_to = "value") %>%
+  ggplot(aes(x = t_i)) + 
+  geom_point(aes(y = Y_i), size = 1) +
+  geom_line(aes(y = value, color = type)) +
+  labs(title = "Brooklyn", x ="Days (since Sep 15, 2020)", y = "Cumulative hospitalizations") +   
+  scale_color_discrete(name = "Fitted model", labels = c("Final", "Start")) 
+
+## Manhattan
+hosp_wave2_mn <- df_hosp_wave2_mn %>%
+  mutate(
+    Y_i_hat_start = N(t_i, start_hosp_wave2_mn),
+    Y_i_hat_final = N(t_i, final_hosp_wave2_mn)
+  ) %>%
+  pivot_longer(Y_i_hat_start:Y_i_hat_final, names_to = "type", values_to = "value") %>%
+  ggplot(aes(x = t_i)) + 
+  geom_point(aes(y = Y_i), size = 1) +
+  geom_line(aes(y = value, color = type)) +
+  labs(title = "Manhattan", x ="Days (since Sep 15, 2020)", y = "Cumulative hospitalizations") +   
+  scale_color_discrete(name = "Fitted model", labels = c("Final", "Start")) 
+
+## Queens
+hosp_wave2_qn <- df_hosp_wave2_qn %>%
+  mutate(
+    Y_i_hat_start = N(t_i, start_hosp_wave2_qn),
+    Y_i_hat_final = N(t_i, final_hosp_wave2_qn)
+  ) %>%
+  pivot_longer(Y_i_hat_start:Y_i_hat_final, names_to = "type", values_to = "value") %>%
+  ggplot(aes(x = t_i)) + 
+  geom_point(aes(y = Y_i), size = 1) +
+  geom_line(aes(y = value, color = type)) +
+  labs(title = "Queens", x ="Days (since Sep 15, 2020)", y = "Cumulative hospitalizations") +   
+  scale_color_discrete(name = "Fitted model", labels = c("Final", "Start")) 
+
+## Staten Island
+hosp_wave2_si <- df_hosp_wave2_si %>%
+  mutate(
+    Y_i_hat_start = N(t_i, start_hosp_wave2_si),
+    Y_i_hat_final = N(t_i, final_hosp_wave2_si)
+  ) %>%
+  pivot_longer(Y_i_hat_start:Y_i_hat_final, names_to = "type", values_to = "value") %>%
+  ggplot(aes(x = t_i)) + 
+  geom_point(aes(y = Y_i), size = 1) +
+  geom_line(aes(y = value, color = type)) +
+  labs(title = "Staten Island", x ="Days (since Sep 15, 2020)", y = "Cumulative hospitalizations") +   
+  scale_color_discrete(name = "Fitted model", labels = c("Final", "Start")) 
+
+## NYC
+hosp_wave2_nyc <- df_hosp_wave2_nyc %>%
+  mutate(
+    Y_i_hat_start = N(t_i, start_hosp_wave2_nyc),
+    Y_i_hat_final = N(t_i, final_hosp_wave2_nyc)
+  ) %>%
+  pivot_longer(Y_i_hat_start:Y_i_hat_final, names_to = "type", values_to = "value") %>%
+  ggplot(aes(x = t_i)) + 
+  geom_point(aes(y = Y_i), size = 1) +
+  geom_line(aes(y = value, color = type)) +
+  labs(title = "New York City", x ="Days (since Sep 15, 2020)", y = "Cumulative hospitalizations") +   
+  scale_color_discrete(name = "Fitted model", labels = c("Final", "Start")) 
+
+
+ggarrange(hosp_wave2_nyc, hosp_wave2_bx, hosp_wave2_bk, hosp_wave2_mn, hosp_wave2_qn, hosp_wave2_si,  nrow = 2, ncol = 3, common.legend = TRUE, legend = "bottom") 
