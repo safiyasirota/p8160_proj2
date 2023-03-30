c90aea6bcf274339f6b658c7d63d84569b683eb4
safiyasirota <90366618+safiyasirota@users.noreply.github.com>
2023-03-30 19:54:04 -0400

    adding predictive plots to wave 2 case code, downloading predictive plot .pngs into the plots folder

--- wave2case.R
+++ wave2case.R
@@ -191,4 +191,187 @@ case_wave2_nyc <- df_wave2_nyc %>%
   scale_color_discrete(name = "Fitted model", labels = c("Final", "Start")) 
 
 
-ggarrange(case_wave2_nyc, case_wave2_bx, case_wave2_bk, case_wave2_mn, case_wave2_qn, case_wave2_si,  nrow = 2, ncol = 3, common.legend = TRUE, legend = "bottom") 
+ggarrange(case_wave2_nyc, case_wave2_bx, case_wave2_bk, case_wave2_mn, 
+          case_wave2_qn, case_wave2_si,  nrow = 2, ncol = 3, 
+          common.legend = TRUE, legend = "bottom") 
+
+# Task 1.3
+
+## NYC extended curve
+more_days <- seq(from = as.Date("2020-12-12"), to = as.Date("2021-06-12"), 
+                 by = 'day')
+more_days <- ymd(more_days)
+currow <- nrow(df_wave2_nyc)
+more_times <- seq(from = currow + 1, 
+                  to = length(more_days) + currow)
+Ys <- rep(NA, length(more_days))
+more_data <- tibble(date = more_days, t_i = more_times, Y_i = Ys)
+
+## Adding data to current data set
+df_wave2_nyc_ext <- rbind(df_wave2_nyc, more_data)
+
+case_wave2_nyc_ext <- df_wave2_nyc_ext %>%
+  mutate(
+    Y_i_hat_final = N(t_i, final_nyc)
+  ) %>%
+  ggplot(aes(x = t_i)) + 
+  geom_point(aes(y = Y_i), size = 1) +
+  geom_line(aes(y = Y_i_hat_final, color = "red")) +
+  labs(title = "New York City", x = "Days (since Sep 15, 2020)", y = "Cumulative cases") +
+  theme(legend.position = "none") +
+  coord_cartesian(xlim = c(0, 300))
+
+## BX extended curve
+
+# Constructing a vector of future dates
+more_days <- seq(from = as.Date("2020-12-12"), to = as.Date("2021-06-12"), 
+                 by = 'day')
+more_days <- ymd(more_days)
+currow <- nrow(df_wave2_bx)
+more_times <- seq(from = currow + 1, 
+                  to = length(more_days) + currow)
+Ys <- rep(NA, length(more_days))
+more_data <- tibble(date = more_days, t_i = more_times, Y_i = Ys)
+
+# Adding data to current data set
+df_wave2_bx_ext <- rbind(df_wave2_bx, more_data)
+
+# Saving population value
+pop_bx <- 1472654
+
+# Making new plot
+case_wave2_bx_ext <- df_wave2_bx_ext %>%
+  mutate(
+    Y_i_hat_final = N(t_i, final_bx)
+  ) %>%
+  ggplot(aes(x = t_i)) + 
+  geom_point(aes(y = (Y_i/pop_bx)*100000), size = 1) +
+  geom_line(aes(y = (Y_i_hat_final/pop_bx)*100000), color = "red") +
+  labs(title = "Bronx", x ="Days (since Sep 15, 2020)", 
+       y = "Cumulative cases per 100,000 capita") +
+  theme(legend.position = "none") +
+  coord_cartesian(xlim = c(0, 300), ylim = c(0, 4500))
+
+## BK extended curve
+
+# Constructing a vector of future dates
+more_days <- seq(from = as.Date("2020-12-12"), to = as.Date("2021-06-12"), 
+                 by = 'day')
+more_days <- ymd(more_days)
+currow <- nrow(df_wave2_bk)
+more_times <- seq(from = currow + 1, 
+                  to = length(more_days) + currow)
+Ys <- rep(NA, length(more_days))
+more_data <- tibble(date = more_days, t_i = more_times, Y_i = Ys)
+
+# Adding data to current data set
+df_wave2_bk_ext <- rbind(df_wave2_bk, more_data)
+
+# Saving population value
+pop_bk <- 2736074
+
+# Making new plot
+case_wave2_bk_ext <- df_wave2_bk_ext %>%
+  mutate(
+    Y_i_hat_final = N(t_i, final_bk)
+  ) %>%
+  ggplot(aes(x = t_i)) + 
+  geom_point(aes(y = (Y_i/pop_bk)*100000), size = 1) +
+  geom_line(aes(y = (Y_i_hat_final/pop_bk)*100000), color = "red") +
+  labs(title = "Brooklyn", x ="Days (since Sep 15, 2020)", 
+       y = "Cumulative cases per 100,000 capita") +
+  theme(legend.position = "none") +
+  coord_cartesian(xlim = c(0, 300), ylim = c(0, 4500))
+
+## MN extended curve
+
+more_days <- seq(from = as.Date("2020-12-12"), to = as.Date("2021-06-12"), 
+                 by = 'day')
+more_days <- ymd(more_days)
+currow <- nrow(df_wave2_mn)
+more_times <- seq(from = currow + 1, 
+                  to = length(more_days) + currow)
+Ys <- rep(NA, length(more_days))
+more_data <- tibble(date = more_days, t_i = more_times, Y_i = Ys)
+
+# Adding data to current data set
+df_wave2_mn_ext <- rbind(df_wave2_mn, more_data)
+
+# Saving population value
+pop_mn <- 1694251
+
+# Making new plot
+case_wave2_mn_ext <- df_wave2_mn_ext %>%
+  mutate(
+    Y_i_hat_final = N(t_i, final_mn)
+  ) %>%
+  ggplot(aes(x = t_i)) + 
+  geom_point(aes(y = (Y_i/pop_mn)*100000), size = 1) +
+  geom_line(aes(y = (Y_i_hat_final/pop_mn)*100000), color = "red") +
+  labs(title = "Manhattan", x ="Days (since Sep 15, 2020)", 
+       y = "Cumulative cases per 100,000 capita") +
+  theme(legend.position = "none") +
+  coord_cartesian(xlim = c(0, 300), ylim = c(0, 4500))
+
+## QN extended curve
+
+more_days <- seq(from = as.Date("2020-12-12"), to = as.Date("2021-06-12"), 
+                 by = 'day')
+more_days <- ymd(more_days)
+currow <- nrow(df_wave2_qn)
+more_times <- seq(from = currow + 1, 
+                  to = length(more_days) + currow)
+Ys <- rep(NA, length(more_days))
+more_data <- tibble(date = more_days, t_i = more_times, Y_i = Ys)
+
+# Adding data to current data set
+df_wave2_qn_ext <- rbind(df_wave2_qn, more_data)
+
+# Saving population value
+pop_qn <- 2405464
+
+# Making new plot
+case_wave2_qn_ext <- df_wave2_qn_ext %>%
+  mutate(
+    Y_i_hat_final = N(t_i, final_qn)
+  ) %>%
+  ggplot(aes(x = t_i)) + 
+  geom_point(aes(y = (Y_i/pop_qn)*100000), size = 1) +
+  geom_line(aes(y = (Y_i_hat_final/pop_qn)*100000), color = "red") +
+  labs(title = "Queens", x ="Days (since Sep 15, 2020)", 
+       y = "Cumulative cases per 100,000 capita") +
+  theme(legend.position = "none") +
+  coord_cartesian(xlim = c(0, 300), ylim = c(0, 4500))
+
+## SI extended curve
+
+# Constructing a vector of future dates
+more_days <- seq(from = as.Date("2020-12-12"), to = as.Date("2021-06-12"), 
+                by = 'day')
+more_days <- ymd(more_days)
+currow <- nrow(df_wave2_si)
+more_times <- seq(from = currow + 1, 
+                  to = length(more_days) + currow)
+Ys <- rep(NA, length(more_days))
+more_data <- tibble(date = more_days, t_i = more_times, Y_i = Ys)
+
+# Adding data to current data set
+df_wave2_si_ext <- rbind(df_wave2_si, more_data)
+
+# Saving population value
+pop_si <- 495747
+
+# Making new plot
+case_wave2_si_ext <- df_wave2_si_ext %>%
+  mutate(
+    Y_i_hat_final = N(t_i, final_si)
+  ) %>%
+  ggplot(aes(x = t_i)) + 
+  geom_point(aes(y = (Y_i/pop_si)*100000), size = 1) +
+  geom_line(aes(y = (Y_i_hat_final/pop_si)*100000), color = "red") +
+  labs(title = "Staten Island", x ="Days (since Sep 15, 2020)", 
+       y = "Cumulative cases per 100,000 capita") +
+  theme(legend.position = "none") +
+  coord_cartesian(xlim = c(0, 300), ylim = c(0, 4500))
+
+
